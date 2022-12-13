e54d81fd63cd5b0f1ac7a64ba849246745445904
kym1924 <kym1924@naver.com>
2022-12-14 00:45:21 +0900

    Update MainActivity for BottomNavigationView

--- app/src/main/java/com/kimym/marvel/MainActivity.kt
+++ app/src/main/java/com/kimym/marvel/MainActivity.kt
@@ -2,10 +2,32 @@ package com.kimym.marvel
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.kimym.marvel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        initBottomNavigationView()
+        initDecorFitsSystemWindows()
+    }
+
+    private fun initBottomNavigationView() {
+        binding.bottomNavigation.setupWithNavController(getNavController())
+    }
+
+    private fun getNavController(): NavController {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController
+    }
+
+    private fun initDecorFitsSystemWindows() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 }
