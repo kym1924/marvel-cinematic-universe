a40952f9410cc995d1225f1cd5bce0030d1c5845
kym1924 <kym1924@naver.com>
2022-12-15 23:52:01 +0900

    Update DatabaseModule for Pre-population the database

--- app/src/main/java/com/kimym/marvel/di/DatabaseModule.kt
+++ app/src/main/java/com/kimym/marvel/di/DatabaseModule.kt
@@ -2,13 +2,19 @@ package com.kimym.marvel.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.kimym.marvel.database.MarvelDao
 import com.kimym.marvel.database.MarvelDatabase
+import com.kimym.marvel.worker.MarvelDatabaseWorker
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import timber.log.Timber
 import javax.inject.Singleton
 
 @Module
@@ -21,6 +27,15 @@ object DatabaseModule {
             context,
             MarvelDatabase::class.java,
             "Marvel.db"
+        ).addCallback(
+            object : RoomDatabase.Callback() {
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+                    val request = OneTimeWorkRequestBuilder<MarvelDatabaseWorker>().build()
+                    WorkManager.getInstance(context).enqueue(request)
+                    Timber.d("Database is initialized")
+                }
+            }
         ).build()
     }
 
