102b18579da8580a988e32cafe2881f5e1122795
kym1924 <kym1924@naver.com>
2022-12-15 23:46:29 +0900

    Update MarvelDao for insertMovies()

--- app/src/main/java/com/kimym/marvel/database/MarvelDao.kt
+++ app/src/main/java/com/kimym/marvel/database/MarvelDao.kt
@@ -1,12 +1,17 @@
 package com.kimym.marvel.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.kimym.marvel.database.model.Movie
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MarvelDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovies(movies: List<Movie>)
+
     @Query("SELECT * FROM Movie")
     fun getMovies(): Flow<List<Movie>>
 }
