f4aa7e77fe330b11d389d1ff7279e749e7dc875a
kym1924 <kym1924@naver.com>
2022-12-15 23:47:30 +0900

    Create MarvelDatabaseWorker

---
+++ app/src/main/java/com/kimym/marvel/worker/MarvelDatabaseWorker.kt
@@ -0,0 +1,38 @@
+package com.kimym.marvel.worker
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
+import com.kimym.marvel.database.MarvelDao
+import com.kimym.marvel.database.model.Movie
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.coroutineScope
+
+@HiltWorker
+class MarvelDatabaseWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted params: WorkerParameters,
+    private val dao: MarvelDao
+) : CoroutineWorker(context, params) {
+    override suspend fun doWork(): Result {
+        return coroutineScope {
+            try {
+                applicationContext.assets.open("marvel.json").use { inputStream ->
+                    JsonReader(inputStream.reader()).use { jsonReader ->
+                        val type = object : TypeToken<List<Movie>>() {}.type
+                        val list: List<Movie> = Gson().fromJson(jsonReader, type)
+                        dao.insertMovies(movies = list)
+                    }
+                }
+                Result.success()
+            } catch (e: Exception) {
+                Result.failure()
+            }
+        }
+    }
+}
