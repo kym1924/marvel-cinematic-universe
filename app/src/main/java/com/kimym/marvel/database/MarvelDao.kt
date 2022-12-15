6405b0c00a60f07527b5f8253a2477a5f31b7956
kym1924 <kym1924@naver.com>
2022-12-15 22:54:58 +0900

    Update MarvelDao for getMovies()

--- app/src/main/java/com/kimym/marvel/database/MarvelDao.kt
+++ app/src/main/java/com/kimym/marvel/database/MarvelDao.kt
@@ -1,6 +1,12 @@
 package com.kimym.marvel.database
 
 import androidx.room.Dao
+import androidx.room.Query
+import com.kimym.marvel.database.model.Movie
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface MarvelDao
+interface MarvelDao {
+    @Query("SELECT * FROM Movie")
+    fun getMovies(): Flow<List<Movie>>
+}
