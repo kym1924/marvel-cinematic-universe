7f82d25f4286099f83e7515152cee59c2983bf01
kym1924 <kym1924@naver.com>
2022-12-15 23:51:21 +0900

    Update MarvelApplication for injection WorkManager with Hilt

--- app/src/main/java/com/kimym/marvel/MarvelApplication.kt
+++ app/src/main/java/com/kimym/marvel/MarvelApplication.kt
@@ -1,7 +1,19 @@
 package com.kimym.marvel
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MarvelApplication : Application()
+class MarvelApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+    }
+}
