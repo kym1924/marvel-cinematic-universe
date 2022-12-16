a130bf3e3f6d6b358624d9ebfddb1d55f805e7ab
kym1924 <kym1924@naver.com>
2022-12-16 14:25:43 +0900

    Update MovieFragment for movies flow collecting

--- app/src/main/java/com/kimym/marvel/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/kimym/marvel/ui/movie/MovieFragment.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.kimym.marvel.databinding.FragmentMovieBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MovieFragment : Fragment() {
     private var _binding: FragmentMovieBinding? = null
     private val binding get() = requireNotNull(_binding)
+    private val viewModel by viewModels<MovieViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,10 +31,21 @@ class MovieFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRecyclerView()
+        initMoviesCollect()
     }
 
     private fun initRecyclerView() {
         binding.rvMovie.also { it.setHasFixedSize(true) }.adapter = MovieAdapter()
+    }
+
+    private fun initMoviesCollect() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.movies.collect { list ->
+                    (binding.rvMovie.adapter as MovieAdapter).submitList(list)
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
