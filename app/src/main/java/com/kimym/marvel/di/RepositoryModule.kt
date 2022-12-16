c65f3a40afed507fe68c3acb1b39688e9eff905d
kym1924 <kym1924@naver.com>
2022-12-16 14:20:53 +0900

    Create RepositoryModule

---
+++ app/src/main/java/com/kimym/marvel/di/RepositoryModule.kt
@@ -0,0 +1,19 @@
+package com.kimym.marvel.di
+
+import com.kimym.marvel.data.MovieRepository
+import com.kimym.marvel.data.MovieRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindMovieRepository(
+        movieRepositoryImpl: MovieRepositoryImpl
+    ): MovieRepository
+}
