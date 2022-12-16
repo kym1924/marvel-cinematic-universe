f5391167322e04dcf4c261053ffdad682a970478
kym1924 <kym1924@naver.com>
2022-12-16 14:22:04 +0900

    Update MainActivity for @AndroidEntryPoint

--- app/src/main/java/com/kimym/marvel/MainActivity.kt
+++ app/src/main/java/com/kimym/marvel/MainActivity.kt
@@ -7,9 +7,12 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.kimym.marvel.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
