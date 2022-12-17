162465f23c06563ad2814632719b41b48dcc8a6f
kym1924 <kym1924@naver.com>
2022-12-18 02:28:50 +0900

    Update DetailViewModel for insertRating()

--- app/src/main/java/com/kimym/marvel/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/kimym/marvel/ui/detail/DetailViewModel.kt
@@ -2,16 +2,24 @@ package com.kimym.marvel.ui.detail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.kimym.marvel.data.repository.DetailRepository
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.launch
 
 class DetailViewModel @AssistedInject constructor(
-    repository: DetailRepository,
+    private val repository: DetailRepository,
     @Assisted private val title: String
 ) : ViewModel() {
     val movie = repository.getMovie(title)
+
+    fun insertRating(rating: Float) {
+        viewModelScope.launch {
+            repository.insertRating(title, rating)
+        }
+    }
 
     @AssistedFactory
     interface DetailAssistedFactory {
