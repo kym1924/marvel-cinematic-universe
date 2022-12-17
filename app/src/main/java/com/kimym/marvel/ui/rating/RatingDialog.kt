3d55ecd896c5c8f7965d697cfb8d2a5872ee4a46
kym1924 <kym1924@naver.com>
2022-12-18 02:16:22 +0900

    Update RatingDialog for RatingBarChangeListener

--- app/src/main/java/com/kimym/marvel/ui/rating/RatingDialog.kt
+++ app/src/main/java/com/kimym/marvel/ui/rating/RatingDialog.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.kimym.marvel.databinding.DialogRatingBinding
 
@@ -24,12 +27,20 @@ class RatingDialog : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initMovieTitle()
+        initRatingBarChangeListener()
     }
 
     private fun initMovieTitle() {
         val args by navArgs<RatingDialogArgs>()
         binding.tvRatingMovieTitle.text = args.title
         binding.executePendingBindings()
+    }
+
+    private fun initRatingBarChangeListener() {
+        binding.ratingBar.setOnRatingBarChangeListener { _, value, _ ->
+            setFragmentResult("rating", bundleOf("rating" to value))
+            findNavController().popBackStack()
+        }
     }
 
     override fun onDestroyView() {
