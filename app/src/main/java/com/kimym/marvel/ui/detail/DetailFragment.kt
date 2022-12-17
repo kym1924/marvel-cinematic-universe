d75193f5d9c4a339f04562d53d90ab4dfd2cc89e
kym1924 <kym1924@naver.com>
2022-12-17 20:36:00 +0900

    Update DetailFragment for Data Binding

--- app/src/main/java/com/kimym/marvel/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/kimym/marvel/ui/detail/DetailFragment.kt
@@ -5,11 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.navArgs
 import com.kimym.marvel.databinding.FragmentDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class DetailFragment : Fragment() {
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = requireNotNull(_binding)
+    private val args by navArgs<DetailFragmentArgs>()
+
+    @Inject
+    lateinit var viewModelAssistedFactory: DetailViewModel.DetailAssistedFactory
+
+    private val viewModel by viewModels<DetailViewModel> {
+        DetailViewModel.provideDetailAssistedFactory(viewModelAssistedFactory, args.title)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +35,22 @@ class DetailFragment : Fragment() {
     ): View {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initMovieCollect()
+    }
+
+    private fun initMovieCollect() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.movie.collect {
+                    binding.movie = it
+                    binding.executePendingBindings()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
