6cb31a6bf42ac68929097a9a3e3f23b626c7edea
kym1924 <kym1924@naver.com>
2022-12-17 18:53:44 +0900

    Update MovieViewModel for Phase

--- app/src/main/java/com/kimym/marvel/ui/movie/MovieViewModel.kt
+++ app/src/main/java/com/kimym/marvel/ui/movie/MovieViewModel.kt
@@ -1,13 +1,31 @@
 package com.kimym.marvel.ui.movie
 
+import androidx.annotation.MenuRes
 import androidx.lifecycle.ViewModel
 import com.kimym.marvel.data.MovieRepository
+import com.kimym.marvel.data.model.Phase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
+@OptIn(ExperimentalCoroutinesApi::class)
 class MovieViewModel @Inject constructor(
     repository: MovieRepository
 ) : ViewModel() {
-    val movies = repository.getMovies()
+    var phase = MutableStateFlow(Phase.ALL)
+        private set
+
+    val movies = phase.flatMapLatest { phase ->
+        when (phase) {
+            Phase.ALL -> repository.getMovies()
+            else -> repository.getMoviesByPhase(phase.ordinal)
+        }
+    }
+
+    fun setPhase(@MenuRes menuId: Int) {
+        Phase.map[menuId]?.let { phase.value = it }
+    }
 }
