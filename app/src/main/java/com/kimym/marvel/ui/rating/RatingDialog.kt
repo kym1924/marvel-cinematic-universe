b5a99f1b0bd5fc9965b4ec09e1f1965b03add878
kym1924 <kym1924@naver.com>
2022-12-17 22:30:45 +0900

    Update RatingDialog for movie title

--- app/src/main/java/com/kimym/marvel/ui/rating/RatingDialog.kt
+++ app/src/main/java/com/kimym/marvel/ui/rating/RatingDialog.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.navArgs
 import com.kimym.marvel.databinding.DialogRatingBinding
 
 class RatingDialog : DialogFragment() {
@@ -18,6 +19,17 @@ class RatingDialog : DialogFragment() {
     ): View {
         _binding = DialogRatingBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initMovieTitle()
+    }
+
+    private fun initMovieTitle() {
+        val args by navArgs<RatingDialogArgs>()
+        binding.tvRatingMovieTitle.text = args.title
+        binding.executePendingBindings()
     }
 
     override fun onDestroyView() {
