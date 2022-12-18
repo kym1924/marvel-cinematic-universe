87678888633cae4626d8b3be38b0da74c47dce7d
kym1924 <kym1924@naver.com>
2022-12-19 02:24:13 +0900

    Update FavoriteFragment for initFavoritesCollect()

--- app/src/main/java/com/kimym/marvel/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/kimym/marvel/ui/favorite/FavoriteFragment.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.kimym.marvel.databinding.FragmentFavoriteBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class FavoriteFragment : Fragment() {
     private var _binding: FragmentFavoriteBinding? = null
     private val binding get() = requireNotNull(_binding)
+    private val viewModel by viewModels<FavoriteViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,10 +31,21 @@ class FavoriteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRecyclerView()
+        initFavoritesCollect()
     }
 
     private fun initRecyclerView() {
         binding.rvFavorite.also { it.setHasFixedSize(true) }.adapter = FavoriteAdapter()
+    }
+
+    private fun initFavoritesCollect() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.favorites.collect { list ->
+                    (binding.rvFavorite.adapter as FavoriteAdapter).submitList(list)
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
