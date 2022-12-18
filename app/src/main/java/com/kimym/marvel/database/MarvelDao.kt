18a72e90e04b57cc676066040a97d572e4a582cb
kym1924 <kym1924@naver.com>
2022-12-19 02:10:21 +0900

    Update MarvelDao for getFavorites()

--- app/src/main/java/com/kimym/marvel/database/MarvelDao.kt
+++ app/src/main/java/com/kimym/marvel/database/MarvelDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.kimym.marvel.data.model.MovieAndRating
 import com.kimym.marvel.data.model.MovieBasicInfo
 import com.kimym.marvel.data.model.MovieDetailInfo
 import com.kimym.marvel.database.model.Movie
@@ -23,6 +24,16 @@ interface MarvelDao {
 
     @Query("SELECT id, title, image FROM Movie WHERE phase = :phase")
     fun getMoviesByPhase(phase: Int): Flow<List<MovieBasicInfo>>
+
+    @Query(
+        """
+            SELECT e.id, e.title, e.image, r.rating
+            FROM Movie AS e, Rating AS r
+            WHERE e.title = r.title
+            ORDER BY r.id
+        """
+    )
+    fun getFavorites(): Flow<List<MovieAndRating>>
 
     @Query("SELECT id, title, content, `release`, running_time AS runningTime, image FROM Movie WHERE title = :title")
     fun getMovie(title: String): Flow<MovieDetailInfo>
