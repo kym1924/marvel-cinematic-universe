d4304fbc64935171ff3fca0e05f61efa86dadab0
kym1924 <kym1924@naver.com>
2022-12-19 02:11:44 +0900

    Update RepositoryModule for FavoriteRepository

--- app/src/main/java/com/kimym/marvel/di/RepositoryModule.kt
+++ app/src/main/java/com/kimym/marvel/di/RepositoryModule.kt
@@ -2,6 +2,8 @@ package com.kimym.marvel.di
 
 import com.kimym.marvel.data.repository.DetailRepository
 import com.kimym.marvel.data.repository.DetailRepositoryImpl
+import com.kimym.marvel.data.repository.FavoriteRepository
+import com.kimym.marvel.data.repository.FavoriteRepositoryImpl
 import com.kimym.marvel.data.repository.MovieRepository
 import com.kimym.marvel.data.repository.MovieRepositoryImpl
 import dagger.Binds
@@ -18,6 +20,12 @@ abstract class RepositoryModule {
     abstract fun bindMovieRepository(
         movieRepositoryImpl: MovieRepositoryImpl
     ): MovieRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindFavoriteRepository(
+        favoriteRepositoryImpl: FavoriteRepositoryImpl
+    ): FavoriteRepository
 
     @Binds
     @Singleton
