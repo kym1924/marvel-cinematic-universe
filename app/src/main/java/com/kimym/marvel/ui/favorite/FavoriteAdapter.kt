d1fa36a7ba55cd2deb8a42287ce6049e971f0ada
kym1924 <kym1924@naver.com>
2022-12-19 02:23:55 +0900

    Update FavoriteAdapter for MovieAndRating

--- app/src/main/java/com/kimym/marvel/ui/favorite/FavoriteAdapter.kt
+++ app/src/main/java/com/kimym/marvel/ui/favorite/FavoriteAdapter.kt
@@ -5,16 +5,16 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.kimym.marvel.database.model.Rating
+import com.kimym.marvel.data.model.MovieAndRating
 import com.kimym.marvel.databinding.ItemFavoriteBinding
 
-class FavoriteAdapter : ListAdapter<Rating, FavoriteAdapter.FavoriteViewHolder>(
-    object : DiffUtil.ItemCallback<Rating>() {
-        override fun areItemsTheSame(oldItem: Rating, newItem: Rating): Boolean {
+class FavoriteAdapter : ListAdapter<MovieAndRating, FavoriteAdapter.FavoriteViewHolder>(
+    object : DiffUtil.ItemCallback<MovieAndRating>() {
+        override fun areItemsTheSame(oldItem: MovieAndRating, newItem: MovieAndRating): Boolean {
             return oldItem.id == newItem.id
         }
 
-        override fun areContentsTheSame(oldItem: Rating, newItem: Rating): Boolean {
+        override fun areContentsTheSame(oldItem: MovieAndRating, newItem: MovieAndRating): Boolean {
             return oldItem == newItem
         }
     }
@@ -31,7 +31,7 @@ class FavoriteAdapter : ListAdapter<Rating, FavoriteAdapter.FavoriteViewHolder>(
 
     class FavoriteViewHolder(private val binding: ItemFavoriteBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(model: Rating) {
+        fun bind(model: MovieAndRating) {
             binding.model = model
             binding.executePendingBindings()
         }
