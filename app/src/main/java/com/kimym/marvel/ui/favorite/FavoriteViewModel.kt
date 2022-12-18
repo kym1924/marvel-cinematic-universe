e99979181efbb2c3773317031b74f365b889f1ca
kym1924 <kym1924@naver.com>
2022-12-19 02:14:11 +0900

    Create FavoriteViewModel

---
+++ app/src/main/java/com/kimym/marvel/ui/favorite/FavoriteViewModel.kt
@@ -0,0 +1,17 @@
+package com.kimym.marvel.ui.favorite
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kimym.marvel.data.repository.FavoriteRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoriteViewModel @Inject constructor(
+    repository: FavoriteRepository
+) : ViewModel() {
+    val favorites = repository.getFavorites()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+}
