b6a39af24d1c530c2d08aae1d171c2e77bd7f7ec
kym1924 <kym1924@naver.com>
2022-12-19 18:36:37 +0900

    Update MovieViewModel for StateFlow

--- app/src/main/java/com/kimym/marvel/ui/movie/MovieViewModel.kt
+++ app/src/main/java/com/kimym/marvel/ui/movie/MovieViewModel.kt
@@ -2,12 +2,16 @@ package com.kimym.marvel.ui.movie
 
 import androidx.annotation.MenuRes
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kimym.marvel.data.model.Phase
 import com.kimym.marvel.data.repository.MovieRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,17 +19,17 @@ import javax.inject.Inject
 class MovieViewModel @Inject constructor(
     repository: MovieRepository
 ) : ViewModel() {
-    var phase = MutableStateFlow(Phase.ALL)
-        private set
+    private val _phase = MutableStateFlow(Phase.ALL)
+    val phase = _phase.asStateFlow()
 
-    val movies = phase.flatMapLatest { phase ->
+    val movies = _phase.flatMapLatest { phase ->
         when (phase) {
             Phase.ALL -> repository.getMovies()
             else -> repository.getMoviesByPhase(phase.ordinal)
         }
-    }
+    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
 
     fun setPhase(@MenuRes menuId: Int) {
-        Phase.map[menuId]?.let { phase.value = it }
+        Phase.map[menuId]?.let { _phase.value = it }
     }
 }
