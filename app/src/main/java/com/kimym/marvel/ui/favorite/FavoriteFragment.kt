bae0e4f71bd6e988a5bc9717bceeef08f332838c
kym1924 <kym1924@naver.com>
2022-12-19 18:37:17 +0900

    Update FavoriteFragment for Data Binding

--- app/src/main/java/com/kimym/marvel/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/kimym/marvel/ui/favorite/FavoriteFragment.kt
@@ -6,12 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.kimym.marvel.databinding.FragmentFavoriteBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class FavoriteFragment : Fragment() {
@@ -24,27 +20,22 @@ class FavoriteFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initRecyclerView()
-        initFavoritesCollect()
+        initBindingVariables()
     }
 
-    private fun initRecyclerView() {
-        binding.rvFavorite.also { it.setHasFixedSize(true) }.adapter = FavoriteAdapter()
-    }
-
-    private fun initFavoritesCollect() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.favorites.collect { list ->
-                    (binding.rvFavorite.adapter as FavoriteAdapter).submitList(list)
-                }
-            }
+    private fun initBindingVariables() {
+        with(binding) {
+            vm = viewModel
+            adapter = FavoriteAdapter()
+            executePendingBindings()
         }
     }
 
