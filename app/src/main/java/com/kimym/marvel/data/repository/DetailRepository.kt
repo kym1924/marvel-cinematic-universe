96399b736d702638d583f5ba0d1c7efbaeca218e
kym1924 <kym1924@naver.com>
2022-12-29 16:23:22 +0900

    Update DetailRepository for deleting unused methods

--- app/src/main/java/com/kimym/marvel/data/repository/DetailRepository.kt
+++ app/src/main/java/com/kimym/marvel/data/repository/DetailRepository.kt
@@ -5,8 +5,4 @@ import kotlinx.coroutines.flow.Flow
 
 interface DetailRepository {
     fun getMovie(title: String): Flow<MovieDetailInfo>
-
-    fun isFavorite(title: String): Flow<Boolean>
-
-    suspend fun insertRating(title: String, rating: Float)
 }
--- app/src/main/java/com/kimym/marvel/data/repository/DetailRepositoryImpl.kt
+++ app/src/main/java/com/kimym/marvel/data/repository/DetailRepositoryImpl.kt
@@ -2,7 +2,6 @@ package com.kimym.marvel.data.repository
 
 import com.kimym.marvel.data.model.MovieDetailInfo
 import com.kimym.marvel.database.MarvelDao
-import com.kimym.marvel.database.model.Rating
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
@@ -14,13 +13,5 @@ class DetailRepositoryImpl @Inject constructor(
 ) : DetailRepository {
     override fun getMovie(title: String): Flow<MovieDetailInfo> {
         return dao.getMovie(title).flowOn(ioDispatcher)
-    }
-
-    override fun isFavorite(title: String): Flow<Boolean> {
-        return dao.getIsFavorite(title)
-    }
-
-    override suspend fun insertRating(title: String, rating: Float) {
-        dao.insertRating(Rating(title = title, rating = rating))
     }
 }
