ee5b1470e5cbad96cea6fa010db102cf8177aa58
kym1924 <kym1924@naver.com>
2023-01-07 18:45:40 +0900

    Create JankStatsExtensions

---
+++ app/src/main/java/com/kimym/marvel/util/JankStatsExtensions.kt
@@ -0,0 +1,32 @@
+package com.kimym.marvel.util
+
+import androidx.databinding.ViewDataBinding
+import androidx.metrics.performance.PerformanceMetricsState
+import androidx.recyclerview.widget.RecyclerView
+
+private const val key = "RecyclerView"
+
+fun ViewDataBinding.getMetricsStateHolder(): PerformanceMetricsState.Holder {
+    return PerformanceMetricsState.getHolderForHierarchy(root)
+}
+
+fun PerformanceMetricsState.Holder.putState(
+    key: String,
+    value: String
+) {
+    state?.putState(key, value)
+}
+
+fun RecyclerView.addOnScrollListenerForJankStats(holder: PerformanceMetricsState.Holder) {
+    addOnScrollListener(
+        object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                when (newState) {
+                    RecyclerView.SCROLL_STATE_DRAGGING -> holder.putState(key, "Dragging")
+                    RecyclerView.SCROLL_STATE_SETTLING -> holder.putState(key, "Settling")
+                    else -> holder.state?.removeState(key)
+                }
+            }
+        }
+    )
+}
