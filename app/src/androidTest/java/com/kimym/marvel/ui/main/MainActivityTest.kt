a9e517f74872ffa64f2c0e2d9c8827473867fba2
kym1924 <kym1924@naver.com>
2023-01-12 19:06:56 +0900

    Update MainActivityTest

--- app/src/androidTest/java/com/kimym/marvel/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/kimym/marvel/ui/main/MainActivityTest.kt
@@ -1,4 +1,4 @@
-package com.kimym.marvel
+package com.kimym.marvel.ui.main
 
 import androidx.core.view.get
 import androidx.navigation.testing.TestNavHostController
@@ -11,7 +11,7 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.kimym.marvel.ui.main.MainActivity
+import com.kimym.marvel.R
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
@@ -19,10 +19,12 @@ import org.junit.Rule
 import org.junit.Test
 
 class MainActivityTest {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    private val menu get() = bottomNavigationView.menu
+
     @get:Rule
     val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
-
-    private lateinit var bottomNavigationView: BottomNavigationView
 
     @Before
     fun setUp() {
@@ -32,27 +34,26 @@ class MainActivityTest {
     }
 
     @Test
-    fun bottomNavigationDisplayedTest() {
+    fun testBottomNavigationViewIsDisplayed() {
         onView(withId(R.id.bottom_navigation)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun bottomNavigationViewMenuTest() {
-        val menu = bottomNavigationView.menu
+    fun testBottomNavigationViewMenu() {
         assertEquals(menu.size(), 2)
         assertEquals(menu[0].itemId, R.id.movieFragment)
         assertEquals(menu[1].itemId, R.id.favoriteFragment)
     }
 
     @Test
-    fun bottomNavigationViewSelectionTest() {
-        assertTrue(bottomNavigationView.menu.findItem(R.id.movieFragment).isChecked)
+    fun testBottomNavigationViewSelection() {
+        assertTrue(menu.findItem(R.id.movieFragment).isChecked)
         onView(withId(R.id.favoriteFragment)).perform(click())
-        assertTrue(bottomNavigationView.menu.findItem(R.id.favoriteFragment).isChecked)
+        assertTrue(menu.findItem(R.id.favoriteFragment).isChecked)
     }
 
     @Test
-    fun bottomNavigationViewWithNavControllerTest() {
+    fun testBottomNavigationViewSelectionWithNavController() {
         val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
 
         activityScenarioRule.scenario.onActivity {
