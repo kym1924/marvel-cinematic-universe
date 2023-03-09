02af28fecb685f6277056372197d72a48fb76fcd
kym1924 <kym1924@naver.com>
2023-03-09 22:22:06 +0900

    Update MarvelDatabaseWorkerTest for WorkManagerTestInitHelper

--- app/src/androidTest/java/com/kimym/marvel/worker/MarvelDatabaseWorkerTest.kt
+++ app/src/androidTest/java/com/kimym/marvel/worker/MarvelDatabaseWorkerTest.kt
@@ -1,10 +1,14 @@
 package com.kimym.marvel.worker
 
+import android.util.Log
 import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.work.Configuration
 import androidx.work.ListenableWorker.Result
+import androidx.work.testing.SynchronousExecutor
 import androidx.work.testing.TestListenableWorkerBuilder
+import androidx.work.testing.WorkManagerTestInitHelper
 import com.kimym.marvel.database.MarvelDao
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -29,6 +33,12 @@ class MarvelDatabaseWorkerTest {
     @Before
     fun setUp() {
         rule.inject()
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val config = Configuration.Builder()
+            .setMinimumLoggingLevel(Log.DEBUG)
+            .setExecutor(SynchronousExecutor())
+            .build()
+        WorkManagerTestInitHelper.initializeTestWorkManager(context, config)
     }
 
     @ExperimentalCoroutinesApi
