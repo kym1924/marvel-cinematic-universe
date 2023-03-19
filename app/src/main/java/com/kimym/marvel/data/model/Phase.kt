e322322e190fd1bfb42b0e60a54da6b7a93909f3
kym1924 <kym1924@naver.com>
2023-03-19 18:08:06 +0900

    Update Phase for deleting unused map

--- app/src/main/java/com/kimym/marvel/data/model/Phase.kt
+++ app/src/main/java/com/kimym/marvel/data/model/Phase.kt
@@ -1,21 +1,9 @@
 package com.kimym.marvel.data.model
 
-import com.kimym.marvel.R
-
 enum class Phase(val str: String) {
     ALL(""),
     ONE("Phase 1"),
     TWO("Phase 2"),
     THREE("Phase 3"),
-    FOUR("Phase 4");
-
-    companion object {
-        val map = mapOf(
-            R.id.filter_phase_all to ALL,
-            R.id.filter_phase_1 to ONE,
-            R.id.filter_phase_2 to TWO,
-            R.id.filter_phase_3 to THREE,
-            R.id.filter_phase_4 to FOUR
-        )
-    }
+    FOUR("Phase 4")
 }
