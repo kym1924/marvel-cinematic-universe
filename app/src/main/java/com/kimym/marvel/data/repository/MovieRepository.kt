3fdb3e10dd75571498a9faaa749bf18b86647836
kym1924 <kym1924@naver.com>
2023-03-19 18:07:27 +0900

    Update MovieRepository for MarvelDatastore

--- app/src/main/java/com/kimym/marvel/data/repository/MovieRepository.kt
+++ app/src/main/java/com/kimym/marvel/data/repository/MovieRepository.kt
@@ -1,10 +1,16 @@
 package com.kimym.marvel.data.repository
 
+import androidx.annotation.MenuRes
 import com.kimym.marvel.data.model.MovieBasicInfo
+import com.kimym.marvel.data.model.Phase
 import kotlinx.coroutines.flow.Flow
 
 interface MovieRepository {
     fun getMovies(): Flow<List<MovieBasicInfo>>
 
     fun getMoviesByPhase(phase: Int): Flow<List<MovieBasicInfo>>
+
+    fun getPhase(): Flow<Phase>
+
+    suspend fun setPhase(@MenuRes menuId: Int)
 }
