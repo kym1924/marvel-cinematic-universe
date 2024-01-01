6e6cddf7f7ba534fadeb74de3344406f124c4cab
kym1924 <kym1924@naver.com>
2024-01-01 17:20:43 +0900

    Update Configuration.Provider for WorkManager 2.9.0

--- app/src/main/java/com/kimym/marvel/MarvelApplication.kt
+++ app/src/main/java/com/kimym/marvel/MarvelApplication.kt
@@ -11,9 +11,8 @@ class MarvelApplication : Application(), Configuration.Provider {
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
 
-    override fun getWorkManagerConfiguration(): Configuration {
-        return Configuration.Builder()
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
             .setWorkerFactory(workerFactory)
             .build()
-    }
 }
