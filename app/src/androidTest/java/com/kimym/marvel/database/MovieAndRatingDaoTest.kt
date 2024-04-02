f7fee173285f827556396ac02990e4d003bad2b0
kym1924 <kym1924@naver.com>
2024-04-02 22:57:35 +0900

    Create MovieAndRatingDaoTest

---
+++ app/src/androidTest/java/com/kimym/marvel/database/MovieAndRatingDaoTest.kt
@@ -0,0 +1,103 @@
+package com.kimym.marvel.database
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kimym.marvel.core.database.MarvelDatabase
+import com.kimym.marvel.core.database.dao.MovieAndRatingDao
+import com.kimym.marvel.core.database.dao.MovieDao
+import com.kimym.marvel.core.database.dao.RatingDao
+import com.kimym.marvel.core.database.entity.MovieEntity
+import com.kimym.marvel.core.database.entity.RatingEntity
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MovieAndRatingDaoTest {
+    private lateinit var db: MarvelDatabase
+    private lateinit var movieDao: MovieDao
+    private lateinit var ratingDao: RatingDao
+    private lateinit var movieAndRatingDao: MovieAndRatingDao
+
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, MarvelDatabase::class.java).build()
+        movieDao = db.movieDao()
+        ratingDao = db.ratingDao()
+        movieAndRatingDao = db.movieAndRatingDao()
+    }
+
+    @After
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    fun movieAndRatingDao_get_movieAndRatings() = runTest {
+        val movies = listOf(
+            movieEntity(0, "Iron Man"),
+            movieEntity(2, "Iron Man 2"),
+            movieEntity(6, "Iron Man 3")
+        )
+
+        movieDao.insertMovies(movies)
+
+        val ratings = listOf(
+            ratingEntity(0, 4.0f),
+            ratingEntity(2, 3.0f),
+            ratingEntity(6, 5.0f)
+        ).onEach { entity ->
+            ratingDao.insertRating(entity)
+        }
+
+        val movieAndRatings = movieAndRatingDao.getMovieAndRatings().first()
+
+        assertThat(
+            movieAndRatings.map { entity -> entity.title },
+            `is`(movies.map { entity -> entity.title })
+        )
+
+        assertThat(
+            movieAndRatings.map { entity -> entity.id },
+            `is`(movies.map { entity -> entity.id })
+        )
+
+        assertThat(
+            movieAndRatings.map { entity -> entity.rating },
+            `is`(ratings.map { entity -> entity.rating })
+        )
+    }
+
+    private fun movieEntity(
+        id: Int,
+        title: String
+    ): MovieEntity {
+        return MovieEntity(
+            id = id,
+            title = title,
+            phase = 0,
+            content = "",
+            release = "",
+            runningTime = 0,
+            image = ""
+        )
+    }
+
+    private fun ratingEntity(
+        id: Int,
+        rating: Float
+    ): RatingEntity {
+        return RatingEntity(
+            id = id,
+            rating = rating
+        )
+    }
+}
