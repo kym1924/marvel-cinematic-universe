4f2718e2ee8ff36b53bfbd740e715cd309993632
kym1924 <kym1924@naver.com>
2024-04-16 22:05:00 +0900

    Delete MarvelDatabasePrePopulateWorkerTest initWorkManager

--- core-worker/src/androidTest/java/com/kimym/marvel/core/MarvelDatabasePrePopulateWorkerTest.kt
+++ core-worker/src/androidTest/java/com/kimym/marvel/core/MarvelDatabasePrePopulateWorkerTest.kt
@@ -1,16 +1,12 @@
 package com.kimym.marvel.core
 
 import android.content.Context
-import android.util.Log
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.work.Configuration
 import androidx.work.ListenableWorker.Result
-import androidx.work.testing.SynchronousExecutor
 import androidx.work.testing.TestListenableWorkerBuilder
-import androidx.work.testing.WorkManagerTestInitHelper
 import com.kimym.marvel.core.database.MarvelDatabase
 import com.kimym.marvel.core.database.dao.MovieDao
 import com.kimym.marvel.core.worker.MarvelDatabasePrePopulateWorker
@@ -28,23 +24,9 @@ class MarvelDatabasePrePopulateWorkerTest {
 
     @Before
     fun setUp() {
-        createDb()
-        initWorkManager()
-    }
-
-    private fun createDb() {
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(context, MarvelDatabase::class.java).build()
         movieDao = db.movieDao()
-    }
-
-    private fun initWorkManager() {
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
-        val config = Configuration.Builder()
-            .setMinimumLoggingLevel(Log.DEBUG)
-            .setExecutor(SynchronousExecutor())
-            .build()
-        WorkManagerTestInitHelper.initializeTestWorkManager(context, config)
     }
 
     @After
