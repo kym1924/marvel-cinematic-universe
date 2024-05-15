76bfb4511ae60feb852e1a924b4a377e4b2797c6
kym1924 <kym1924@naver.com>
2024-05-15 15:27:31 +0900

    Create MarvelDatastore

--- core-datastore/src/main/java/com/kimym/marvel/core/datastore/MarvelDatastore.kt
+++ core-datastore/src/main/java/com/kimym/marvel/core/datastore/MarvelDatastore.kt
@@ -1,12 +1,32 @@
 package com.kimym.marvel.core.datastore
 
 import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-private val Context.datastore by preferencesDataStore("marvel_data_store")
+private val Context.datastore by preferencesDataStore("marvel_store")
 
-class MarvelDatastore @Inject constructor(
-    @ApplicationContext private val context: Context
-)
+class MarvelDatastore @Inject constructor(@ApplicationContext context: Context) {
+    private val datastore = context.datastore
+
+    fun getAppearance(): Flow<Int?> {
+        return datastore.data.map { preferences ->
+            preferences[appearance]
+        }
+    }
+
+    suspend fun setAppearance(value: Int) {
+        datastore.edit { preferences ->
+            preferences[appearance] = value
+        }
+    }
+
+    companion object {
+        private val appearance = intPreferencesKey("appearance_key")
+    }
+}
