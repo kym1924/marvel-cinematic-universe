91a3a6804406f525e421c2dac8ed1d927c5bf2af
kym1924 <kym1924@naver.com>
2024-07-03 09:50:07 +0900

    Update BindingAdapters for Toolbar.setSettingMenuClickListener

--- core-ui/src/main/java/com/kimym/marvel/core/ui/BindingAdapters.kt
+++ core-ui/src/main/java/com/kimym/marvel/core/ui/BindingAdapters.kt
@@ -1,7 +1,11 @@
 package com.kimym.marvel.core.ui
 
 import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.navigation.NavDeepLinkRequest
+import androidx.navigation.findNavController
 import com.bumptech.glide.Glide
 
 @BindingAdapter("setImageWithUrl")
@@ -10,5 +14,20 @@ fun ImageView.setImageWithUrl(url: String?) {
         Glide.with(context)
             .load(url)
             .into(this)
+    }
+}
+
+@BindingAdapter("navigate")
+fun Toolbar.setSettingMenuClickListener(url: String) {
+    setOnMenuItemClickListener { menu ->
+        when (menu.itemId) {
+            R.id.menu_setting -> {
+                val request = NavDeepLinkRequest.Builder
+                    .fromUri(url.toUri())
+                    .build()
+                findNavController().navigate(request)
+            }
+        }
+        true
     }
 }
